9133cb1ea3678859829a89fa942b2e315354c1fc
gonzalesr <rolandogonzales@outlook.com>
2025-02-03 00:07:07 -0400

    TIPatientPostman

--- Patient/PatientManagement.Infrastructure/obj/Debug/net8.0/PatientManagement.Infrastructure.AssemblyInfo.cs
+++ Patient/PatientManagement.Infrastructure/obj/Debug/net8.0/PatientManagement.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f89005883cd175eaab72cd2a2f8b1a1743c2d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1897e8bddd4a73aec8c226b98407c985d5bf24d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
